15869201a8635a27e9d337ff55d7d345a30dd28b
Follett <clf9008@gmail.com>
2020-11-01 09:43:38 -0700

    updated script.js with inIT function to declare constants in global memory

---
+++ script.js
@@ -0,0 +1,15 @@
+function initPage() {
+    const  inputE1 = document.getElementById("location-input");
+    const  searchE1 = document.getElementById("location-search");
+    const  clearE1 = document.getElementById("clear-location");
+    const  nameE1 = document.getElementById("location-name");
+    const  currentPicE1 = document.getElementById("location-pic");
+    const  currentTempE1 = document.getElementById("temp");
+    const  currentHumidtyE1 = document.getElementById("humidity");
+    const  currentWindE1 = document.getElementById("wind-speed");
+    const  currentUVIndexE1 = document.getElementById("UV-index");
+    const  historyE1 = document.getElementById("history");
+    let locationHistory = JSON.parse(localStorage.getItem("search")) || [];
+
+    console.log(searchHistory);
+}
